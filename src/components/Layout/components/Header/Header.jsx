8f3ebc827084091f881e0542c357c5c28608ed16
Tran Phu Son <phuson2809@gmail.com>
2022-09-27 16:47:11 +0700

    Layout default

--- src/components/Layout/components/Header/Header.jsx
+++ src/components/Layout/components/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles); //giúp viết đc dấu gạch ngang khi đặt tên className
 
 function Header() {
-  return <div>Header</div>;
+  return <header className={cx('wrapper')}>
+    <div className={cx('inner')}></div>
+  </header>;
 }
 
 export default Header;
